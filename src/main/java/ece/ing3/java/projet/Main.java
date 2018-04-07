fff6dafd96b3f255a77e379359b3a6d1a024c49a
lfgaleota <louisfelix@galeota.fr>
2018-04-07 19:17:48 +0200

    Modification du Main pour initialiser la configuration, la base de donnée et rediriger sur la REPL

--- src/main/java/ece/ing3/java/projet/Main.java
+++ src/main/java/ece/ing3/java/projet/Main.java
@@ -1,7 +1,14 @@
 package ece.ing3.java.projet;
 
-public class Main {
-    public static void main( String[] args ) {
+import ece.ing3.java.projet.configuration.Configuration;
+import ece.ing3.java.projet.database.Database;
+import ece.ing3.java.projet.utils.REPL;
 
-    }
+public class Main {
+	public static void main( String[] args ) throws Exception {
+		Configuration.init();
+		Database.init();
+		REPL.main( args );
+		Database.close();
+	}
 }
