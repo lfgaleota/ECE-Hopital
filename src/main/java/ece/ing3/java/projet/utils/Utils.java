795d2d384c565d40db4897136e3a8cfafcd97669
horgeon <me@horgeon.fr>
2018-04-07 19:12:55 +0200

    Ajout de classes utilitaires

---
+++ src/main/java/ece/ing3/java/projet/utils/Utils.java
@@ -0,0 +1,33 @@
+package ece.ing3.java.projet.utils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Paths;
+
+/**
+ * Utility functions
+ */
+public class Utils {
+	/**
+	 * Get a resource's full path from a path relative to either the execution directory or the JAR
+	 *
+	 * @param path Relative resource path
+	 * @return Resource's full path
+	 * @throws FileNotFoundException Resource not found at the provided path
+	 */
+	public static File getResourcePath( String path ) throws FileNotFoundException {
+		try {
+			URL fileUrl = Utils.class.getClassLoader().getResource( path );
+			if( fileUrl == null )
+				throw new FileNotFoundException( "Resource '" + path + "' not found." );
+
+			URI fileUri = fileUrl.toURI();
+			return Paths.get( fileUri ).toFile();
+		} catch( URISyntaxException e ) {
+			throw new FileNotFoundException( "Resource '" + path + "' not found." + e.getLocalizedMessage() );
+		}
+	}
+}
