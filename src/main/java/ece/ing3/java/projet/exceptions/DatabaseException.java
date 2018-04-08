69011cf53fee31681d52a1b408714aa491a8be2d
lfgaleota <louisfelix@galeota.fr>
2018-04-08 20:08:53 +0200

    Correction JavaDoc

--- src/main/java/ece/ing3/java/projet/exceptions/DatabaseException.java
+++ src/main/java/ece/ing3/java/projet/exceptions/DatabaseException.java
@@ -5,21 +5,30 @@ package ece.ing3.java.projet.exceptions;
  */
 public class DatabaseException extends Exception {
 	/**
-	 * {@link Exception#Exception(String, Throwable)}
+	 * Constructs a new exception with the specified detail message and cause.
+	 * <p>
+	 * Note that the detail message associated with cause is not automatically incorporated in this exception's detail message.
+	 *
+	 * @param message the detail message (which is saved for later retrieval by the {@link Throwable#getMessage()} method).
+	 * @param cause   the cause (which is saved for later retrieval by the {@link Throwable#getCause()} method). (A {@code null} value is permitted, and indicates that the cause is nonexistent or unknown.)
 	 */
 	public DatabaseException( String message, Throwable cause ) {
 		super( message, cause );
 	}
 
 	/**
-	 * {@link Exception#Exception(String)}
+	 * Constructs a new exception with the specified detail message. The cause is not initialized, and may subsequently be initialized by a call to {@link Throwable#initCause(Throwable)}
+	 *
+	 * @param message the detail message. The detail message is saved for later retrieval by the {@link Throwable#getMessage()} method.
 	 */
 	public DatabaseException( String message ) {
 		super( message );
 	}
 
 	/**
-	 * {@link Exception#Exception(Throwable)}
+	 * Constructs a new exception with the specified cause and a detail message of {@code (cause==null ? null : cause.toString())} (which typically contains the class and detail message of {@code cause}). This constructor is useful for exceptions that are little more than wrappers for other throwables (for example, {@link java.security.PrivilegedActionException})
+	 *
+	 * @param cause the cause (which is saved for later retrieval by the {@link Throwable#getCause()} method). (A {@code null} value is permitted, and indicates that the cause is nonexistent or unknown.)
 	 */
 	public DatabaseException( Throwable cause ) {
 		super( cause );
