1a6807ebf9bc92e39b5b1c00120aa0bdb413405a
VirgileDujet <38044199+VirgileDujet@users.noreply.github.com>
2018-04-09 10:32:54 +0200

    UI de base mais avec partie boutons indépendante.

--- src/main/java/ece/ing3/java/projet/Main.java
+++ src/main/java/ece/ing3/java/projet/Main.java
@@ -1,7 +1,13 @@
 package ece.ing3.java.projet;
 
+import ece.ing3.java.projet.vue.Fenetre;
+
 public class Main {
     public static void main( String[] args ) {
 
+        
+        Fenetre fenetre = new Fenetre();
+   
+        
     }
 }
