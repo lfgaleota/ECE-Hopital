a06feebe345997122b63f23ad866945ca97f609e
Nicolas33333 <Nicolas33333@users.noreply.github.com>
2018-04-09 11:08:31 +0200

    Ajout du logger d'interface et du logo

--- src/main/java/ece/ing3/java/projet/utils/Constants.java
+++ src/main/java/ece/ing3/java/projet/utils/Constants.java
@@ -5,4 +5,6 @@ package ece.ing3.java.projet.utils;
  */
 public class Constants {
 	public static final String RESOURCE_PATH_CONFIGURATION = "config.properties";
+	public static final String RESOURCE_PATH_APPLOGO = "imgs/logo.png";
+	public static final String LOGGER_NAME_UI = "UI";
 }
