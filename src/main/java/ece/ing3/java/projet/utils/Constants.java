92e07cf26d4090de0dcd3a87e005a36c99a1850c
Nicolas33333 <Nicolas33333@users.noreply.github.com>
2018-04-14 20:05:45 +0200

    Ajout de constantes pour les identifier les images de la barres de recherche du haut.

--- src/main/java/ece/ing3/java/projet/utils/Constants.java
+++ src/main/java/ece/ing3/java/projet/utils/Constants.java
@@ -6,6 +6,11 @@ package ece.ing3.java.projet.utils;
 public class Constants {
 	public static final String RESOURCE_PATH_CONFIGURATION = "config.properties";
 	public static final String RESOURCE_PATH_APPLOGO = "imgs/logo.png";
+	public static final String RESOURCE_PATH_CONFIGLOGO = "imgs/config.jpg";
+	public static final String RESOURCE_PATH_AJOUTERLOGO = "imgs/ajouter.jpg";
+	public static final String RESOURCE_PATH_MODIFIERLOGO = "imgs/modifier.jpg";
+	public static final String RESOURCE_PATH_RECHERCHERLOGO = "imgs/rechercher.jpg";
+	public static final String RESOURCE_PATH_SUPPRIMERLOGO = "imgs/supprimer.jpg";
 	public static final String LOGGER_NAME_UI = "UI";
 	public static final Integer UI_TEXTFIELD_MAXLENGTH = 255;
 }
