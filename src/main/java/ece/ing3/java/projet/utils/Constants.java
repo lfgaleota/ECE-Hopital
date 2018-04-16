6df1c7bdd1b6a0571c680b07d608bb937c3f95e4
horgeon <me@horgeon.fr>
2018-04-16 18:02:41 +0200

    Centralisation des messages de l'application

--- src/main/java/ece/ing3/java/projet/utils/Constants.java
+++ src/main/java/ece/ing3/java/projet/utils/Constants.java
@@ -5,24 +5,28 @@ package ece.ing3.java.projet.utils;
  */
 public class Constants {
 	public static final String RESOURCE_PATH_CONFIGURATION = "config.properties";
-	
-	public static final String RESOURCE_PATH_APPLOGO = "src/main/resources/imgs/logo.png";
-	public static final String RESOURCE_PATH_CONFIGLOGO = "src/main/resources/imgs/config.jpg";
-	public static final String RESOURCE_PATH_STATLOGO = "src/main/resources/imgs/statistiques.png";
-	public static final String RESOURCE_PATH_AJOUTERLOGO = "src/main/resources/imgs/ajouter.png";
-	public static final String RESOURCE_PATH_MODIFIERLOGO = "src/main/resources/imgs/modifier.png";
-	public static final String RESOURCE_PATH_RECHERCHERLOGO = "src/main/resources/imgs/rechercher.png";
-	public static final String RESOURCE_PATH_SUPPRIMERLOGO = "src/main/resources/imgs/supprimer.png";
-	public static final String RESOURCE_PATH_SERVICELOGO = "src/main/resources/imgs/services.png";
-	public static final String RESOURCE_PATH_PATIENTLOGO = "src/main/resources/imgs/patients.png";
-	public static final String RESOURCE_PATH_CHAMBRELOGO = "src/main/resources/imgs/chambres.png";
-	public static final String RESOURCE_PATH_EMPLOYELOGO = "src/main/resources/imgs/employes.png";
-	public static final String RESOURCE_PATH_HOSPITALISATIONLOGO = "src/main/resources/imgs/hospitalisations.png";
-        public static final String RESOURCE_PATH_HOMELOGO = "src/main/resources/imgs/accueil.png";
-	
+
+	public static final String RESOURCE_PATH_APPLOGO = "imgs/logo.png";
+	public static final String RESOURCE_PATH_CONFIGLOGO = "imgs/config.jpg";
+	public static final String RESOURCE_PATH_STATLOGO = "imgs/statistiques.png";
+	public static final String RESOURCE_PATH_AJOUTERLOGO = "imgs/ajouter.png";
+	public static final String RESOURCE_PATH_MODIFIERLOGO = "imgs/modifier.png";
+	public static final String RESOURCE_PATH_RECHERCHERLOGO = "imgs/rechercher.png";
+	public static final String RESOURCE_PATH_SUPPRIMERLOGO = "imgs/supprimer.png";
+	public static final String RESOURCE_PATH_SERVICELOGO = "imgs/services.png";
+	public static final String RESOURCE_PATH_PATIENTLOGO = "imgs/patients.png";
+	public static final String RESOURCE_PATH_CHAMBRELOGO = "imgs/chambres.png";
+	public static final String RESOURCE_PATH_EMPLOYELOGO = "imgs/employes.png";
+	public static final String RESOURCE_PATH_HOSPITALISATIONLOGO = "imgs/hospitalisations.png";
+	public static final String RESOURCE_PATH_HOMELOGO = "imgs/accueil.png";
+
 	public static final String LOGGER_NAME_UI = "UI";
 	public static final Integer UI_TEXTFIELD_MAXLENGTH = 255;
-	
+	public static final Integer UI_TAB_MARGIN_TOP = 110;
+	public static final Integer UI_TAB_MARGIN_LEFT = 5;
+
+	public static final String MODEL_STRINGS_PREFIX = "model.";
+
 	public static final Integer WIDTH_FRAME = 1100;
 	public static final Integer HEIGHT_FRAME = 800;
 }
