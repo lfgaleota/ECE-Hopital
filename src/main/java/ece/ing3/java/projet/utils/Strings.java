936f8c80219f4298a05187edf73c7385f5cd722e
Horgeon <me@horgeon.fr>
2018-04-17 22:19:27 +0200

    Ajout des chaînes manquantes

--- src/main/java/ece/ing3/java/projet/utils/Strings.java
+++ src/main/java/ece/ing3/java/projet/utils/Strings.java
@@ -12,6 +12,27 @@ public class Strings {
 	public static final String MODEL_ADRESSE = "Adresse";
 	public static final String MODEL_NUMEROTELEPHONE = "N° téléphone";
 	public static final String MODEL_MUTUELLE = "Mutuelle";
+	public static final String MODEL_SERVICE = "Service";
+	public static final String MODEL_CODE = "Code";
+	public static final String MODEL_BATIMENT = "Bâtiment";
+	public static final String MODEL_NUMERODIRECTEUR = "N° directeur";
+	public static final String MODEL_EMPLOYE = "Employé";
+	public static final String MODEL_DOCTEUR = "Docteur";
+	public static final String MODEL_SPECIALITE = "Spécialité";
+	public static final String MODEL_ROTATION = "Rotation";
+	public static final String MODEL_SALAIRE = "Salaire";
+	public static final String MODEL_CODESERVICE = "Code du service";
+	public static final String MODEL_INFIRMIER = "Infirmier";
+	public static final String MODEL_CHAMBRE = "Chambre";
+	public static final String MODEL_NUMEROCHAMBRE = "N° chambre";
+	public static final String MODEL_NOMBRELITS = "Nombre de lits";
+	public static final String MODEL_NUMEROSURVEILLANT = "N° surveillant";
+	public static final String MODEL_CODESERVICERATTACHE = "Code du service rattaché";
+	public static final String MODEL_HOSPITALISATION = "Hospitalisation";
+	public static final String MODEL_NUMEROMALADE = "N° malade";
+	public static final String MODEL_NUMEROLIT = "N° lit";
+	public static final String MODEL_SOIGNE = "Prise en charge";
+	public static final String MODEL_NUMERODOCTEUR = "N° docteur";
 
 	private static final Map<String, String> map = new HashMap<>();
 
