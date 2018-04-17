3375b28e793ef01af2f337b7453683e1ac417a96
VirgileDujet <38044199+VirgileDujet@users.noreply.github.com>
2018-04-17 18:33:50 +0200

    Etendue EmployeSearchDialogCpntroller+ Utilisations des PieChart avec base de donnée

--- src/main/java/ece/ing3/java/projet/vue/PieChart2D.java
+++ src/main/java/ece/ing3/java/projet/vue/PieChart2D.java
@@ -1,5 +1,7 @@
 package ece.ing3.java.projet.vue;
 
+import ece.ing3.java.projet.modele.employe.Employe;
+import java.util.List;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
@@ -16,17 +18,53 @@ public class PieChart2D {
 	private static DefaultPieDataset dataset;
 	private static JFreeChart chart;
 	private static ChartPanel chartpanel;
+        private static String tmpdebut;
 
 	/**
 	 * Constructeur par défaut -> Complete les informations du diagramme 
 	 * -> Rempli et affiche le diagramme circulaire dans un panel
 	 */
-	public PieChart2D() {
+	public PieChart2D(List<Employe> malisteemploye) {
 		dataset = new DefaultPieDataset();
-		dataset.setValue("Category 1", 43.2);
-		dataset.setValue("Category 2", 27.9);
-		dataset.setValue("Category 3", 79.5);
+                
+                /*
+                
+                ///A SAVOIR QUE CE BLOC NE SERT UNIQUEMENT POUR DES TEST
+                ///VALEUR DU NOMBRE DE PERSONNE DELA POPLUATION
+                
+                int populationglobale = malisteemploye.size();
+                
+                
+                int populationvoulue=0;
+                
+                
+                System.out.println(malisteemploye.size());
+                
+                for(int i=0;i<malisteemploye.size();i++)
+                {
+                        System.out.println(malisteemploye.get(i).getNom());
+                        
+                        if(malisteemploye.get(i).getNom().charAt(0)=='D')
+                        {
+                            populationvoulue++;
+                        }
+                        
+                }
+   
+                
+                System.out.println(" "+ populationvoulue);
+                
+                
+                
+                
+		dataset.setValue("Category 1",(populationvoulue*100/populationglobale));
+		dataset.setValue("Category 2", (1-populationvoulue/populationglobale)*100);
+		//dataset.setValue("Category 3", 79.5);
 
+                
+                System.out.println("PUTAIN "+malisteemploye.get(0).getAdresse()+" "+ malisteemploye.size());
+                
+                */
 		chart = ChartFactory.createPieChart("Sample Pie Chart", dataset, true, // legend?
 				true, // tooltips?
 				false // URLs?
