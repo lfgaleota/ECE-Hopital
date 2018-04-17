46db36b2a4061bcd29d18e7e8c0994064dfbf9a1
lfgaleota <louisfelix@galeota.fr>
2018-04-17 23:11:37 +0200

    Correction de bugs et restructuration des utilitaires BDD

--- src/main/java/ece/ing3/java/projet/database/sql/queries/SQLRequest.java
+++ src/main/java/ece/ing3/java/projet/database/sql/queries/SQLRequest.java
@@ -2,6 +2,8 @@ package ece.ing3.java.projet.database.sql.queries;
 
 import ece.ing3.java.projet.database.sql.Model;
 
+import java.util.List;
+
 /**
  * Base SQL request interface.
  */
@@ -14,4 +16,11 @@ public interface SQLRequest {
 	 * @return Generated SQL query
 	 */
 	String toString();
+
+	/**
+	 * Gets all the request's parameters
+	 *
+	 * @return Request parameters
+	 */
+	List<Object> getParameters();
 }
