c31dcc694552231aa8feb4735b49cf73a758909d
Horgeon <me@horgeon.fr>
2018-04-17 23:31:27 +0200

    Correction constante invalide

--- src/main/java/ece/ing3/java/projet/vue/panels/InfirmierPanel.java
+++ src/main/java/ece/ing3/java/projet/vue/panels/InfirmierPanel.java
@@ -4,10 +4,9 @@ import ece.ing3.java.projet.modele.employe.Infirmier;
 import ece.ing3.java.projet.modele.tables.TableModel;
 import ece.ing3.java.projet.utils.Strings;
 
-public class InfirmierPanel extends ModelPanel<Infirmier>{
-	public InfirmierPanel(TableModel<Infirmier> tableModel)
-	{
-		super(Strings.getModel("service"), tableModel);
+public class InfirmierPanel extends ModelPanel<Infirmier> {
+	public InfirmierPanel( TableModel<Infirmier> tableModel ) {
+		super( Strings.getModel( "infirmier" ), tableModel );
 	}
 
 }
--- src/main/java/ece/ing3/java/projet/vue/panels/ServicePanel.java
+++ src/main/java/ece/ing3/java/projet/vue/panels/ServicePanel.java
@@ -4,9 +4,8 @@ import ece.ing3.java.projet.modele.administration.Service;
 import ece.ing3.java.projet.modele.tables.TableModel;
 import ece.ing3.java.projet.utils.Strings;
 
-public class ServicePanel extends ModelPanel<Service>{
-		public ServicePanel(TableModel<Service> tableModel)
-		{
-			super(Strings.getModel("service"), tableModel);
-		}
+public class ServicePanel extends ModelPanel<Service> {
+	public ServicePanel( TableModel<Service> tableModel ) {
+		super( Strings.getModel( "service" ), tableModel );
+	}
 }
