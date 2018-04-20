80a544f009e0142544b0297ef0cbc0381644e705
Horgeon <me@horgeon.fr>
2018-04-20 23:31:33 +0200

    Ajout d'icônes dans la barre d'outils

--- src/main/java/ece/ing3/java/projet/vue/components/ModelToolbar.java
+++ src/main/java/ece/ing3/java/projet/vue/components/ModelToolbar.java
@@ -1,19 +1,27 @@
 package ece.ing3.java.projet.vue.components;
 
+import ece.ing3.java.projet.utils.Constants;
+import ece.ing3.java.projet.utils.Utils;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 
 public class ModelToolbar extends JToolBar {
 	private JButton buttonRemove, buttonAdd, buttonModify, buttonSearch, buttonStats, buttonRefresh;
 
 	public ModelToolbar( String modelName ) {
-		buttonRemove = new JButton( "Supprimer" );
-		buttonAdd = new JButton( "Ajouter" );
-		buttonModify = new JButton( "Modifier" );
-		buttonSearch = new JButton( "Rechercher" );
-		buttonStats = new JButton( "Statistiques" );
-		buttonRefresh = new JButton( "Rafraichir" );
+		try {
+			buttonRemove = new JButton( new ImageIcon( Utils.getImageResource( Constants.RESOURCE_PATH_ICON_REMOVE ) ) );
+			buttonAdd = new JButton( new ImageIcon( Utils.getImageResource( Constants.RESOURCE_PATH_ICON_ADD ) ) );
+			buttonModify = new JButton( new ImageIcon( Utils.getImageResource( Constants.RESOURCE_PATH_ICON_MODIFY ) ) );
+			buttonSearch = new JButton( new ImageIcon( Utils.getImageResource( Constants.RESOURCE_PATH_ICON_SEARCH ) ) );
+			buttonStats = new JButton( new ImageIcon( Utils.getImageResource( Constants.RESOURCE_PATH_ICON_STATS ) ) );
+			buttonRefresh = new JButton( new ImageIcon( Utils.getImageResource( Constants.RESOURCE_PATH_ICON_REFRESH ) ) );
+		} catch( IOException e ) {
+			throw new RuntimeException( "Resource not found.\n" + e.getLocalizedMessage(), e );
+		}
 
 		JLabel label = new JLabel( modelName );
 		label.setFont( new Font( label.getName(), Font.BOLD, 18 ) );
